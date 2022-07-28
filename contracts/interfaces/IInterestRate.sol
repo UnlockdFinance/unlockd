376e15423ceb08bb303233df1abc2c25abbf9589
adri pajares <apajaresaguilera@gmail.com>
2022-07-28 13:24:59 +0200

    Comment interfaces

--- contracts/interfaces/IInterestRate.sol
+++ contracts/interfaces/IInterestRate.sol
@@ -7,10 +7,25 @@ pragma solidity 0.8.4;
  * @author Unlockd
  */
 interface IInterestRate {
+  /**
+   * @dev Get the variable borrow rate
+   * @return the base variable borrow rate
+   **/
   function baseVariableBorrowRate() external view returns (uint256);
 
+  /**
+   * @dev Get the maximum variable borrow rate
+   * @return the maximum variable borrow rate
+   **/
   function getMaxVariableBorrowRate() external view returns (uint256);
 
+  /**
+   * @dev Calculates the interest rates depending on the reserve's state and configurations
+   * @param reserve The address of the reserve
+   * @param availableLiquidity The available liquidity for the reserve
+   * @param totalVariableDebt The total borrowed from the reserve at a variable rate
+   * @param reserveFactor The reserve portion of the interest that goes to the treasury of the market
+   **/
   function calculateInterestRates(
     address reserve,
     uint256 availableLiquidity,
@@ -18,6 +33,15 @@ interface IInterestRate {
     uint256 reserveFactor
   ) external view returns (uint256, uint256);
 
+  /**
+   * @dev Calculates the interest rates depending on the reserve's state and configurations
+   * @param reserve The address of the reserve
+   * @param uToken The uToken address
+   * @param liquidityAdded The liquidity added during the operation
+   * @param liquidityTaken The liquidity taken during the operation
+   * @param totalVariableDebt The total borrowed from the reserve at a variable rate
+   * @param reserveFactor The reserve portion of the interest that goes to the treasury of the market
+   **/
   function calculateInterestRates(
     address reserve,
     address uToken,
