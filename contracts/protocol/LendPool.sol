74534916c0e206dc3023a0f4e00100aa59dd1a69
adri pajares <apajaresaguilera@gmail.com>
2022-09-19 10:37:46 +0200

    Audit fix (HAL-14): remove _verifyCallResult from LendPool

--- contracts/protocol/LendPool.sol
+++ contracts/protocol/LendPool.sol
@@ -1027,27 +1027,6 @@ contract LendPool is
     }
   }
 
-  function _verifyCallResult(
-    bool success,
-    bytes memory returndata,
-    string memory errorMessage
-  ) internal pure returns (bytes memory) {
-    if (success) {
-      return returndata;
-    } else {
-      // Look for revert reason and bubble it up if present
-      if (returndata.length > 0) {
-        // The easiest way to bubble the revert reason is using memory via assembly
-        assembly {
-          let returndata_size := mload(returndata)
-          revert(add(32, returndata), returndata_size)
-        }
-      } else {
-        revert(errorMessage);
-      }
-    }
-  }
-
   function _buildLendPoolVars() internal view returns (DataTypes.ExecuteLendPoolStates memory) {
     return DataTypes.ExecuteLendPoolStates({pauseStartTime: _pauseStartTime, pauseDurationTime: _pauseDurationTime});
   }
