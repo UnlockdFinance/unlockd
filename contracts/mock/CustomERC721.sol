2f415cc1cec724b81249dfb5aef1ccbbdd53b560
fepvenancio <filipeepv@gmail.com>
2023-03-13 16:41:59 +0000

    lockeys deployed

--- contracts/mock/CustomERC721.sol
+++ contracts/mock/CustomERC721.sol
@@ -13,7 +13,7 @@ contract CustomERC721 is ERC721Enumerable {
   error MaxSupplyReached();
   error MaxNFTsPerWalletReached();
 
-  uint16 public _currTokenId;
+  uint16 public _currTokenId = 1;
   string public baseURI;
   address private owner;
   uint256 public constant MAX_SUPPLY = 10_000;
@@ -21,7 +21,6 @@ contract CustomERC721 is ERC721Enumerable {
   constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     baseURI = "";
     owner = _msgSender();
-    _currTokenId = 0;
   }
 
   /**
